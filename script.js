0638d6b83dfadc525751f793bd61c5e98ebaa6ff
maysafeliciano-bit <maysa.feliciano@escola.pr.gov.br>
2026-09-04 17:56:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let botoes = document.querySelectorAll("button");
+
+botoes.forEach(function (botao) {
+    let curtiu = false;
+
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        let texto = botao.querySelector("span");
+
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+        } else {
+            texto.textContent--;
+            curtiu = false;
+        }
+    }
+});
 
